3fdc0deb45f00d7b6bed7d445169596a921a0f5e
Melanie Kuhn <melanie@jebbit.com>
2022-12-03 23:05:16 -0500

    Initial Commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,16 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    let title = props.title;
+
     const clickHandler = () => {
-        console.log("clicked")
+        title = "Updated title";
     }
     return (
         <Card className="expense-item">
             <ExenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
